318eb6322fafa8a49e7484a4911ef71bb278bdba
Sanghyun <dev.sanghyun@gmail.com>
2024-06-09 18:20:41 +0900

    CHORE: 프로젝트 생성 및 초기 설정 구성

---
+++ JampleWidget/JampleWidget/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  JampleWidget
+//
+//  Created by James on 6/9/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello, JampleWidget!")
+    }
+    
+}
